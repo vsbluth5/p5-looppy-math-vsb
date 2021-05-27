5eb80cadb0b9dccf6cdfdc32b1f3fb2412f69eb6
Glitch (grey-mountainous-oregano) <none>
2021-05-27 03:25:09 +0000

    🛤👷 Checkpoint ./loops_start.js:3856783/958 ./loops.js:3856783/563

--- loops_start.js
+++ loops_start.js
@@ -1,31 +1,22 @@
 // 1. Print out every number 1-100.
-for (let i = 1; i <= 100; i++) console.log(i);
+
 
 // 2. Print out every number from 50-100.
-for (let i = 50; i <= 100; i++) console.log(i);
+
 
 // 3. Find the sum of the first 100 numbers.
-let sum = 0;
-for (let i = 1; i <= 100; i++) {
-  sum += i;
-}
-console.log(sum);
+
 
 // 4. Find the sum of the numbers from 1000 to 2000.
 
+
 // 5. Print out every perfect square number less than or equal to 100.
-for (let i = 1; i*i <= 100; i++) {
-  console.log(i*i);
-}
 
 
 // 6. If you got one grain of rice on day 1, two grains of rice on day 2, and four grains of rice on day 3, and it kept doubling like that, print out a daily report of how much rice you'd receive on each day for 30 days.
 
 
 // 7. Print out every number that ends in a 7 from 7-1007.
-for (let i = 7; i <= 1007; i=i+10) {
-  console.log(i);
-}
 
 
 // 8. Print out every even number from 1-100.
